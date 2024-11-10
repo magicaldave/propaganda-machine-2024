d485dc156e56960e7fa8676d37ec43cce130f300
Dave Corley <smokydave@icloud.com>
2024-11-10 11:46:42 -0600

    FIX: Correct shader & music application

--- Scripts/zhac/tfor_player.lua
+++ Scripts/zhac/tfor_player.lua
@@ -2,8 +2,6 @@ local ambient = require('openmw.ambient')
 local postprocessing = require('openmw.postprocessing')
 local self = require('openmw.self')
 
-local musicStop = true
-local musicStopped = false
 local musicFileName = "Sound\\PD\\Music\\5-minutes-of-silence.mp3"
 
 local shaderOn = false
@@ -11,7 +9,6 @@ local shaderDataFog
 local shaderDataFade
 local uStrength = 1.0            -- Start fully enabled
 local lerpSpeed = 0.2            -- Adjust this value for the fade-out speed
-local shaderFadeComplete = false -- Track if fade-out is complete
 
 local isInModSpace = false
 
@@ -21,6 +18,7 @@ local function enableFadeShader()
     shaderDataFade:enable()
     shaderDataFade:setFloat("uStrength", uStrength)
 end
+
 local function enableFogShader()
     shaderOn = true
     shaderDataFog = postprocessing.load("PD_Fog")
@@ -28,42 +26,38 @@ local function enableFogShader()
     shaderDataFog:setFloat("uFogHeight",10000)
     shaderDataFog:setFloat("uFogDensity",0.001)
 end
+
 local function disableFogShader()
     if shaderDataFog then
         shaderDataFog:disable()
         shaderDataFog = nil
     end
 end
+
 local function disableFadeShader()
-    if shaderDataFade then
-        shaderDataFade:disable()
-        shaderDataFade = nil
-    end
-    --shaderOn = false
-    shaderFadeComplete = true -- Mark fade-out as complete
-   -- enableFogShader()
+    if not shaderDataFade then return end
+
+    shaderDataFade:disable()
+    shaderDataFade = nil
 end
+
 local cellNamespace = "Fields of Regret"
 local function startsWith(str, prefix) --Checks if a string starts with another string
     return string.sub(str, 1, string.len(prefix)) == prefix
 end
 local function onCellChange(newCell)
-    if startsWith(newCell.name, cellNamespace) then
-        isInModSpace = true
-        --entered TFOR
-
-        -- Enable shader only if it hasn’t been faded out yet
-        if not shaderOn and not shaderFadeComplete then
+    isInModSpace = startsWith(newCell.name, cellNamespace)
+    if isInModSpace then
             enableFadeShader()
             enableFogShader()
-        end
     else
-        isInModSpace = false
+        ambient.stopMusic()
         disableFogShader()
     end
 end
+
 local lastCellId
-local function onUpdate(dt)
+local function onUpdate()
     if self.cell.id ~= lastCellId then
         onCellChange(self.cell)
     end
@@ -72,11 +66,10 @@ end
 
 local function onFrame(dt)
     -- Check and handle music playback
-    if isInModSpace then
-        if musicStop and ambient.isMusicPlaying() and not ambient.isSoundPlaying(musicFileName) then
-            ambient.streamMusic(musicFileName)
-        end
+    if isInModSpace and ambient.isMusicPlaying() and not ambient.isSoundPlaying(musicFileName) then
+        ambient.streamMusic(musicFileName)
     end
+
     -- Gradually decrease uStrength down to 0.0, then disable the shader
     if shaderOn and shaderDataFade then
         uStrength = math.max(0.0, uStrength - dt * lerpSpeed) -- Lerp down to 0.0
@@ -95,13 +88,11 @@ return {
         onSave = function ()
             return {
                 isInModSpace = isInModSpace,
-                shaderFadeComplete = shaderFadeComplete,
             }
         end,
         onLoad = function (data)
             if data then
                 isInModSpace = data.isInModSpace
-                shaderFadeComplete = data.shaderFadeComplete
             end
         end
     }
