b6bac6481ebfa5cb2b9e40763ee9293705cf039d
Dave Corley <smokydave@icloud.com>
2024-11-12 02:19:40 -0600

    FEAT: Refactor shader & music handling (close #10)

--- Scripts/zhac/tfor_player.lua
+++ Scripts/zhac/tfor_player.lua
@@ -4,96 +4,146 @@ local self = require('openmw.self')
 
 local musicFileName = "Sound\\PD\\Music\\5-minutes-of-silence.mp3"
 
-local shaderOn = false
-local shaderDataFog
+local prevCellId
+
 local shaderDataFade
-local uStrength = 1.0            -- Start fully enabled
-local lerpSpeed = 0.2            -- Adjust this value for the fade-out speed
+local shaderDataFog
+local shaderOn = false
 
-local isInModSpace = false
+local lerpSpeed = 0.2
+local uStrength = 1.0
 
-local function enableFadeShader()
+local saveData = {
+    isInModSpace = false
+}
+
+local shaderConfig = {
+    ['Fields of Regret'] = {
+        fade = {
+            uStrength = 1.0
+        },
+        fog = {
+            uFogHeight = 10000,
+            uFogDensity = .001
+        },
+    },
+    ['The Vile Playground'] = {
+        fade = {
+            uStrength = 1.0
+        },
+        fog = {
+            uFogHeight = 10000,
+            uFogDensity = .001
+        },
+    },
+}
+
+local function enableShader(isFog, clavCell)
+    assert(isFog ~= nil, "Cannot enable shader without specifying shader type!")
+    assert(clavCell ~= nil, "Cannot enable shader without specifying the associated PD Cell!")
+
     shaderOn = true
-    shaderDataFade = postprocessing.load("PD_Fade")
-    shaderDataFade:enable()
-    shaderDataFade:setFloat("uStrength", uStrength)
+
+    if isFog then
+        shaderDataFog = postprocessing.load("PD_Fog")
+        shaderDataFog:enable(2)
+
+        for attr, value in pairs(shaderConfig[clavCell].fog) do
+            shaderDataFade:setFloat(attr, value)
+        end
+    else
+        uStrength = shaderConfig[clavCell].fade.uStrength
+        shaderDataFade = postprocessing.load("PD_Fade")
+        shaderDataFade:enable()
+
+        for attr, value in pairs(shaderConfig[clavCell].fade) do
+            shaderDataFade:setFloat(attr, value)
+        end
+    end
 end
 
-local function enableFogShader()
-    shaderOn = true
-    shaderDataFog = postprocessing.load("PD_Fog")
-    shaderDataFog:enable(2)
-    shaderDataFog:setFloat("uFogHeight",10000)
-    shaderDataFog:setFloat("uFogDensity",0.001)
+local function enableShaders(clavCell)
+    assert(shaderConfig[clavCell] ~= nil, "Cannot enable PD shaders for a non-PD or unspecified cell!")
+    enableShader(false, clavCell)
+    enableShader(true, clavCell)
 end
 
-local function disableFogShader()
-    if shaderDataFog then
+local function disableShader(isFog)
+    assert(shaderOn == true, "Cannot disable shaders as they are already off!")
+    assert((isFog and shaderDataFog) or (not isFog and shaderDataFade)
+        , "Cannot disable", (isFog and 'fog' or 'fade'), "shader, it is already off!")
+
+    if isFog and shaderDataFog then
         shaderDataFog:disable()
         shaderDataFog = nil
+    elseif not isFog and shaderDataFade then
+        shaderDataFade:disable()
+        shaderDataFade = nil
+    end
+
+    if not shaderDataFade and not shaderDataFog then shaderOn = false end
+end
+
+local function disableShaders()
+    assert(shaderOn == true, "Cannot disable shaders if they are already off!")
+    if shaderDataFog then disableShader(true) end
+    if shaderDataFade then disableShader(false) end
+end
+
+local function inModSpace(cellName)
+    for pdCell, _ in pairs(shaderConfig) do
+        if string.find(cellName, pdCell) then return pdCell end
     end
 end
 
-local function disableFadeShader()
-    if not shaderDataFade then return end
+local function onCellChange(cellName)
+    local clavRealmCell = inModSpace(cellName)
 
-    shaderDataFade:disable()
-    shaderDataFade = nil
-end
-
-local cellNamespace = "Fields of Regret"
-local function startsWith(str, prefix) --Checks if a string starts with another string
-    return string.sub(str, 1, string.len(prefix)) == prefix
-end
-local function onCellChange(newCell)
-    isInModSpace = startsWith(newCell.name, cellNamespace)
-    if isInModSpace then
-            enableFadeShader()
-            enableFogShader()
+    if clavRealmCell then
+        enableShaders(clavRealmCell)
+        if ambient.isMusicPlaying() then ambient.streamMusic(musicFileName) end
+        saveData.isInModSpace = true
     else
-        ambient.stopMusic()
-        disableFogShader()
+        if saveData.isInModSpace and ambient.isMusicPlaying then
+            ambient.stopMusic()
+            saveData.isInModSpace = false
+        end
+        if shaderOn then disableShaders() end
     end
 end
 
-local lastCellId
 local function onUpdate()
-    if self.cell.id ~= lastCellId then
-        onCellChange(self.cell)
+    if self.cell.id ~= prevCellId then
+        onCellChange(self.cell.name)
     end
-    lastCellId = self.cell.id
+    prevCellId = self.cell.id
+end
+
+local function fadeIn(dt)
+    if not shaderOn or not shaderDataFade then return end
+
+    uStrength = math.max(0.0, uStrength - dt * lerpSpeed)
+
+    if uStrength > 0.0 then
+        shaderDataFade:setFloat("uStrength", uStrength)
+    else
+        disableShader(false)
+    end
 end
 
 local function onFrame(dt)
-    -- Check and handle music playback
-    if isInModSpace and ambient.isMusicPlaying() and not ambient.isSoundPlaying(musicFileName) then
-        ambient.streamMusic(musicFileName)
-    end
-
-    -- Gradually decrease uStrength down to 0.0, then disable the shader
-    if shaderOn and shaderDataFade then
-        uStrength = math.max(0.0, uStrength - dt * lerpSpeed) -- Lerp down to 0.0
-        shaderDataFade:setFloat("uStrength", uStrength)
-
-        if uStrength <= 0.0 then
-            disableFadeShader() -- Disable shader once uStrength reaches 0.0
-        end
-    end
+    fadeIn(dt)
 end
 
 return {
     engineHandlers = {
         onFrame = onFrame,
         onUpdate = onUpdate,
-        onSave = function ()
-            return {
-                isInModSpace = isInModSpace,
-            }
-        end,
+        onSave = function () return saveData end,
         onLoad = function (data)
-            if data then
-                isInModSpace = data.isInModSpace
-            end
-        end
+            if not data then return end
+
+            saveData.isInModSpace = data.isInModSpace or false
+        end,
     }
 }
